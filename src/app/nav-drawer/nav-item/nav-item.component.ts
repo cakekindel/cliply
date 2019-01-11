2a082db3786ab4060ef666f9dece2357cd863dba
Orion Kindel <cakekindel@gmail.com>
2019-01-10 23:46:35 -0700

    Update NavItemComponent to use NavItem

--- src/app/nav-drawer/nav-item/nav-item.component.ts
+++ src/app/nav-drawer/nav-item/nav-item.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NavItem } from './nav-item.model';
 
 @Component({
     selector: 'app-nav-item',
     templateUrl: './nav-item.component.html',
     styleUrls: ['./nav-item.component.scss'],
     host: {
-        '[class.selected]': '_selected'
+        '[class.selected]': 'navItem.selected'
     }
 })
 export class NavItemComponent implements OnInit {
-    private _selected = false;
-
-    @Input() icon: string;
-    @Input() set selected(val: string) {
-        this._selected = true;
-    }
+    @Input() navItem: NavItem;
 
     constructor() { }
 
