12817583b16b091f4173f9412f25c3508c5b64ba
Orion Kindel <cakekindel@gmail.com>
2019-01-10 23:30:57 -0700

    Add nav-drawer

---
+++ src/app/nav-drawer/nav-drawer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-nav-drawer',
+    templateUrl: './nav-drawer.component.html',
+    styleUrls: ['./nav-drawer.component.scss']
+})
+export class SidebarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
