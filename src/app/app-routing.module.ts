8e307ea24d2882341e31f7aa0407e3261c0d169b
Orion Kindel <cakekindel@gmail.com>
2019-01-11 00:27:28 -0700

    Add queue component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QueueComponent } from './queue/queue.component';
 
 const routes: Routes = [
-    // {
-    //     path: '',
-    //     component: HomeComponent
-    // }
+    {
+        path: '',
+        component: QueueComponent
+    }
 ];
 
 @NgModule({
