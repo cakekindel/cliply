e6f0064cb662b8cc78ca3d661ac0b9f0e80a0825
Orion Kindel <cakekindel@gmail.com>
2019-01-20 10:33:28 -0700

    morph-from timing and style tweaks to make more robust

--- src/app/shared/morph-from.directive.ts
+++ src/app/shared/morph-from.directive.ts
@@ -9,6 +9,7 @@ export class MorphFromDirective {
     private morphed = false;
     private readonly fadeDuration = 200;
     private destElement: HTMLElement;
+    private destElementAttrs: { top: number, left: number, width: number, height: number };
 
     @Input('morphFrom') srcElement: HTMLElement;
     @Input() set morph(shouldMorph: boolean) {
@@ -19,12 +20,9 @@ export class MorphFromDirective {
         this.destElement = destElementRef.nativeElement;
 
         this.renderer.addClass(this.destElement, 'morph-hidden');
-
-        timer(1500).subscribe(() => this.morphHandler(true));
-        timer(3500).subscribe(() => this.morphHandler(false));
     }
 
-    morphHandler(shouldMorph: boolean) {
+    private morphHandler(shouldMorph: boolean) {
         if (shouldMorph && !this.morphed) {
             this.morphElements();
             this.morphed = true;
@@ -34,43 +32,20 @@ export class MorphFromDirective {
         }
     }
 
-    unmorphElements() {
-        const morphTimer = timer(0, 200).pipe(takeWhile(step => step <= 5));
+    private morphElements() {
+        const morphTimer = timer(0, this.fadeDuration).pipe(takeWhile(step => step <= 3));
+
+        this.destElementAttrs = {
+            top: this.destElement.offsetTop,
+            left: this.destElement.offsetLeft,
+            width: this.destElement.offsetWidth,
+            height: this.destElement.offsetHeight,
+        };
 
         morphTimer.subscribe(
             step => {
                 switch (step) {
-                    case 1:
-                        this.fadeOutContents(this.destElement);
-                        break;
-                    case 2:
-                        // move destelement back
-                        this.setDestElementStylesToSrcElement();
-                        break;
-                    case 3:
-                        // do nothing; transition lasts 400ms, 2 steps
-                        break;
-                    case 4:
-                        // show srcelement and fade destelement out
-                        this.renderer.removeClass(this.srcElement, 'morph-hidden');
-                        this.renderer.addClass(this.destElement, 'fade-out-fast');
-
-                        break;
-                    case 5:
-                        this.fadeInContents(this.srcElement);
-                        break;
-                }
-            }
-        );
-    }
-
-    morphElements() {
-        const morphTimer = timer(0, 200).pipe(takeWhile(step => step <= 5));
-
-        morphTimer.subscribe(
-            step => {
-                switch (step) {
-                    case 1:
+                    case 0:
                         // set contents hidden so it's just the bg that fades in
                         this.setContentsHidden(this.destElement);
                         // set to be identical to srcelement
@@ -79,27 +54,50 @@ export class MorphFromDirective {
                         this.fadeOutContents(this.srcElement);
                         this.renderer.addClass(this.destElement, 'fade-in-fast');
                         break;
-                    case 2:
+                    case 1:
                         // hide src and set dest to transition
                         this.renderer.addClass(this.srcElement, 'morph-hidden');
                         this.renderer.addClass(this.destElement, 'morphable');
 
                         // move destelement to where it goes
-                        this.renderer.removeAttribute(this.destElement, 'style');
+                        this.setDestElementStyles();
 
                         // cleanup classes
                         this.renderer.removeClass(this.destElement, 'fade-in-fast');
                         this.renderer.removeClass(this.destElement, 'morph-hidden');
                         break;
-                    case 3:
-                        // do nothing; transition lasts 400ms, 2 steps
-                        break;
-                    case 4:
+                    case 2:
                         this.fadeInContents(this.destElement);
                         break;
-                    case 5:
+                    case 3:
                         // cleanup
                         this.resetContents(this.destElement);
+                        break;
+                }
+            }
+        );
+    }
+    private unmorphElements() {
+        const unmorphTimer = timer(0, this.fadeDuration).pipe(takeWhile(step => step <= 3));
+
+        unmorphTimer.subscribe(
+            step => {
+                switch (step) {
+                    case 0:
+                        this.fadeOutContents(this.destElement);
+                        this.setDestElementStyles();
+                        break;
+                    case 1:
+                        // move destelement back
+                        this.setDestElementStylesToSrcElement();
+                        break;
+                    case 2:
+                        // show srcelement and fade destelement out
+                        this.renderer.removeClass(this.srcElement, 'morph-hidden');
+                        this.renderer.addClass(this.destElement, 'fade-out-fast');
+                        break;
+                    case 3:
+                        this.fadeInContents(this.srcElement);
                         break;
                 }
             }
@@ -110,14 +108,26 @@ export class MorphFromDirective {
         this.renderer.setAttribute(
             this.destElement,
             'style',
-            `left: ${this.srcElement.offsetLeft}px;
+            `position: absolute;
+            left: ${this.srcElement.offsetLeft}px;
             top: ${this.srcElement.offsetTop}px;
             width: ${this.srcElement.offsetWidth}px;
             height: ${this.srcElement.offsetHeight}px;`
         );
     }
+    private setDestElementStyles() {
+        this.renderer.setAttribute(
+            this.destElement,
+            'style',
+            `position: absolute;
+            left: ${this.destElementAttrs.left}px;
+            top: ${this.destElementAttrs.top}px;
+            width: ${this.destElementAttrs.width}px;
+            height: ${this.destElementAttrs.height}px;`
+        );
+    }
 
-    setContentsHidden(el: HTMLElement) {
+    private setContentsHidden(el: HTMLElement) {
         const children = Array.from(el.children);
 
         // fade out contents while leaving background
@@ -125,7 +135,7 @@ export class MorphFromDirective {
             this.renderer.addClass(child, 'morph-hidden');
         });
     }
-    resetContents(el: HTMLElement) {
+    private resetContents(el: HTMLElement) {
         const children = Array.from(el.children);
 
         // fade out contents while leaving background
@@ -135,7 +145,7 @@ export class MorphFromDirective {
             this.renderer.removeClass(child, 'fade-out-fast');
         });
     }
-    fadeOutContents(el: HTMLElement) {
+    private fadeOutContents(el: HTMLElement) {
         const children = Array.from(el.children);
 
         // fade out contents while leaving background
@@ -144,7 +154,7 @@ export class MorphFromDirective {
             this.renderer.addClass(child, 'fade-out-fast');
         });
     }
-    fadeInContents(el: HTMLElement) {
+    private fadeInContents(el: HTMLElement) {
         const children = Array.from(el.children);
 
         // fade in contents while leaving background
@@ -154,3 +164,5 @@ export class MorphFromDirective {
         });
     }
 }
+
+export const MORPH_DURATION_MS = 600;
