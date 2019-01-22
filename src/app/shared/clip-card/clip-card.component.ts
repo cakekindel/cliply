a88c3fe1e997d4ccb50bdfe8715170032b8f2f03
Orion Kindel <cakekindel@gmail.com>
2019-01-22 08:54:23 -0700

    Update ClipCardComp to use EditClipSvc

--- src/app/shared/clip-card/clip-card.component.ts
+++ src/app/shared/clip-card/clip-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter }
 import { Clip } from '../models/clip.model';
 import { timer } from 'rxjs';
 import { MORPH_DURATION_MS } from '../morph-from.directive';
+import { EditClipService } from './edit-clip.service';
 
 @Component({
     selector: 'app-clip-card',
@@ -9,37 +10,13 @@ import { MORPH_DURATION_MS } from '../morph-from.directive';
     styleUrls: ['./clip-card.component.scss']
 })
 export class ClipCardComponent implements OnInit {
-    renderEdit = false;
-    showEdit = false;
-    showEditScrim = false;
-
     @Input() clip: Clip;
-    @Output() editing = new EventEmitter<() => void>();
 
     @ViewChild('clip_thumbnail') clipThumbElement: ElementRef<HTMLVideoElement>;
 
-    constructor() { }
+    constructor(public editClipService: EditClipService) { }
 
     ngOnInit() { }
-
-    editClip() {
-        this.renderEdit = true;
-        this.showEdit = true;
-        this.showEditScrim = true;
-        this.editing.emit(() => { this.closeEdit(); });
-    }
-
-    closeEdit() {
-        this.showEdit = false;
-
-        timer(MORPH_DURATION_MS).subscribe(() => {
-            this.showEditScrim = false;
-        });
-
-        timer(MORPH_DURATION_MS + 200).subscribe(() => {
-            this.renderEdit = false;
-        });
-    }
 
     thumbnailMouseover() {
         this.clipThumbElement.nativeElement.muted = true;
