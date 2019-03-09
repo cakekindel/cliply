6adfd973607a6bdb4ed8dc32223ebc4dadb1a31d
Orion Kindel <cakekindel@gmail.com>
2019-03-09 15:09:08 -0700

    Update thumb-hover-preview: add content

--- src/app/shared/clip-card/thumb-hover-preview/thumb-hover-preview.component.ts
+++ src/app/shared/clip-card/thumb-hover-preview/thumb-hover-preview.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Clip } from '../../models/clip.model';
 
 @Component({
   selector: 'app-thumb-hover-preview',
   templateUrl: './thumb-hover-preview.component.html',
   styleUrls: ['./thumb-hover-preview.component.scss']
 })
-export class ThumbHoverPreviewComponent implements OnInit {
+export class ThumbHoverPreviewComponent {
+    public hovering = false;
 
-  constructor() { }
+    @Input() clip = new Clip();
+    @ViewChild('video_preview') videoPreviewElementRef?: ElementRef<HTMLVideoElement>;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    @HostListener('mouseover')
+    onMouseOver() {
+        this.hovering = true;
+
+        if (this.videoPreviewElementRef) {
+            this.videoPreviewElementRef.nativeElement.currentTime = 0;
+            this.videoPreviewElementRef.nativeElement.muted = true;
+            this.videoPreviewElementRef.nativeElement.play();
+        }
+    }
+
+    @HostListener('mouseleave')
+    onMouseLeave() {
+        this.hovering = false;
+    }
 }
