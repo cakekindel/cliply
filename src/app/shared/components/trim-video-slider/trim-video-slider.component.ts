f2cbf8e69e995ffb9469ed5470804a76befda2ae
Orion Kindel <cakekindel@gmail.com>
2019-03-30 00:10:44 -0700

    More changes to slider

--- src/app/shared/components/trim-video-slider/trim-video-slider.component.ts
+++ src/app/shared/components/trim-video-slider/trim-video-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
 import * as moment from 'moment';
 
@@ -20,6 +20,9 @@ import * as moment from 'moment';
     ],
 })
 export class TrimVideoSliderComponent implements OnInit {
+    private _sliderClientLeft = 0;
+    private _sliderWidth = 0;
+
     @Input() duration = 0;
     @Output() startAtChange = new EventEmitter<number>();
     @Output() endAtChange = new EventEmitter<number>();
@@ -27,6 +30,7 @@ export class TrimVideoSliderComponent implements OnInit {
     @ViewChild('startThumbRef') startThumbRef?: ElementRef<HTMLDivElement>;
     @ViewChild('endThumbRef') endThumbRef?: ElementRef<HTMLDivElement>;
     @ViewChild('activeRunnerRef') activeRunnerRef?: ElementRef<HTMLDivElement>;
+    @ViewChild('sliderRef') sliderRef?: ElementRef<HTMLDivElement>;
 
     private _durationMoment = moment.duration(this.duration);
 
@@ -34,22 +38,67 @@ export class TrimVideoSliderComponent implements OnInit {
         positionPercent: 50,
         timeStamp: '0:00',
         elementRef: this.startThumbRef,
+        showLabel: false,
     };
 
     endThumb: SliderThumb = {
-        positionPercent: 0,
+        positionPercent: 100,
         timeStamp: this._durationMoment.minutes() + ':' + this._durationMoment.seconds(),
         elementRef: this.endThumbRef,
+        showLabel: false,
     };
 
-    thumbMouseDown(thumb: SliderThumb, event: Event) { }
-    thumbMouseUp(thumb: SliderThumb, event: Event) { }
+    constructor(private zone: NgZone) { }
 
-    constructor() { }
+    thumbPointerDown(thumb: SliderThumb, event: PointerEvent) {
+        thumb.showLabel = true;
+
+        if (this.sliderRef) {
+            const clientRect = this.sliderRef.nativeElement.getClientRects()[0];
+            this._sliderClientLeft = clientRect.left;
+            this._sliderWidth = clientRect.width;
+        }
+
+        if (thumb.elementRef) {
+            thumb.elementRef.nativeElement.onpointermove = this.onPointerMove(thumb);
+            thumb.elementRef.nativeElement.setPointerCapture(event.pointerId);
+        }
+    }
+
+    thumbPointerUp(thumb: SliderThumb, event: PointerEvent) {
+        thumb.showLabel = false;
+
+        if (thumb.elementRef) {
+            thumb.elementRef.nativeElement.onpointermove = null;
+            thumb.elementRef.nativeElement.releasePointerCapture(event.pointerId);
+        }
+    }
+
+    onPointerMove(thumb: SliderThumb) {
+        return (event: PointerEvent) => {
+            this.zone.run(() => {
+                thumb.positionPercent = ((event.clientX - this._sliderClientLeft) / this._sliderWidth) * 100;
+
+                if (thumb.positionPercent < 0) {
+                    thumb.positionPercent = 0;
+                } else if (thumb.positionPercent > 100) {
+                    thumb.positionPercent = 100;
+                }
+            });
+        };
+    }
 
     ngOnInit() {
         if (this.duration) {
             this._durationMoment = moment.duration(this.duration);
+        }
+
+        if (this.startThumbRef) {
+            this.startThumb.elementRef = this.startThumbRef;
+        }
+
+        if (this.endThumbRef) {
+            this.endThumb.elementRef = this.endThumbRef;
         }
     }
 }
@@ -58,4 +107,5 @@ interface SliderThumb {
     positionPercent: number;
     elementRef?: ElementRef<HTMLDivElement>;
     timeStamp: string;
+    showLabel: boolean;
 }
